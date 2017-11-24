6b76d344503ebd8fd8499895be8ddb54c1c44de2
GitEdvard <edvard.englund@medsci.uu.se>
2017-11-24 16:03:01 +0100

    Update db proc, triggers and view

--- database_objects/BookKeeping_devel_triggers/T_post_insert_update.sql
+++ database_objects/BookKeeping_devel_triggers/T_post_insert_update.sql
@@ -48,7 +48,8 @@ INSERT INTO post_history
 	 purchase_order_no,
 	 sales_order_no,
 	 place_of_purchase_id,
-	 customer_number_id)
+	 customer_number_id,
+	 attention_flag)
 SELECT
 	 post_id,
 	 article_number_id,
@@ -79,7 +80,8 @@ SELECT
 	 purchase_order_no,
 	 sales_order_no,
 	 place_of_purchase_id,
-	 customer_number_id
+	 customer_number_id,
+	 attention_flag
 FROM inserted
 	
 SET NOCOUNT OFF
