b2d881ca7b995b021bfb5affe3c2e0292590dc44
Edvard Englund <edvard.englund@medsci.uu.se>
2021-02-22 13:57:50 +0100

    Add SearchPanel including toggle button

---
+++ PlattformOrdMan/UI/Component/SearchPanel.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PlattformOrdMan.UI.Component
+{
+    partial class SearchPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toggleButton1 = new ToggleButton(this.components);
+            this.LinrPanel = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.splitContainer1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 246);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(3, 16);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.toggleButton1);
+            this.splitContainer1.Panel1.Controls.Add(this.LinrPanel);
+            this.splitContainer1.Size = new System.Drawing.Size(475, 227);
+            this.splitContainer1.SplitterDistance = 102;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Advanced";
+            // 
+            // toggleButton1
+            // 
+            this.toggleButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.toggleButton1.Location = new System.Drawing.Point(7, 75);
+            this.toggleButton1.Name = "toggleButton1";
+            this.toggleButton1.Size = new System.Drawing.Size(25, 23);
+            this.toggleButton1.TabIndex = 0;
+            this.toggleButton1.Text = "\\/";
+            this.toggleButton1.UseVisualStyleBackColor = true;
+            // 
+            // LinrPanel
+            // 
+            this.LinrPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LinrPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LinrPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinrPanel.Location = new System.Drawing.Point(105, 89);
+            this.LinrPanel.Name = "LinrPanel";
+            this.LinrPanel.Size = new System.Drawing.Size(365, 2);
+            this.LinrPanel.TabIndex = 6;
+            // 
+            // SearchPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SearchPanel";
+            this.Size = new System.Drawing.Size(487, 246);
+            this.groupBox1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel LinrPanel;
+        private ToggleButton toggleButton1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PlattformOrdMan/UI/Component/SearchPanel.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+
+namespace PlattformOrdMan.UI.Component
+{
+    public partial class SearchPanel : UserControl
+    {
+        public delegate void ExpandEvent();
+
+        public delegate void CollapseEvent();
+
+        public event ExpandEvent SearchboxExpanded;
+        public event CollapseEvent SearchboxCollapsed;
+
+        private readonly int _panel2OrigHeight;
+        private readonly int _splitterDistance;
+
+        public string Caption
+        {
+            get => groupBox1.Text;
+            set => groupBox1.Text = value;
+        }
+
+        public SearchPanel()
+        {
+            InitializeComponent();
+            _panel2OrigHeight = splitContainer1.Panel2.Height;
+            _splitterDistance = splitContainer1.SplitterDistance;
+            LinrPanel.Visible = false;
+            toggleButton1.SplitterExpanded += OnSplitterExpanded;
+            toggleButton1.SplitterCollapsed += OnSplitterCollapsed;
+            toggleButton1.OnClick(null, null);
+        }
+
+        private void OnSplitterCollapsed()
+        {
+            splitContainer1.Panel2Collapsed = true;
+            groupBox1.Height -= _panel2OrigHeight;
+            splitContainer1.SplitterDistance = _splitterDistance;
+            LinrPanel.Visible = true;
+            SearchboxCollapsed?.Invoke();
+        }
+
+        private void OnSplitterExpanded()
+        {
+            splitContainer1.Panel2Collapsed = false;
+            groupBox1.Height += _panel2OrigHeight;
+            splitContainer1.SplitterDistance = _splitterDistance;
+            LinrPanel.Visible = false;
+            SearchboxExpanded?.Invoke();
+        }
+
+    }
+}
